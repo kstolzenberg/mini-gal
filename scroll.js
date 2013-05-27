7a4618d0656b6988a295c87976fd0847ba9ba6bd
Karen <karen@Karens-MacBook-Air.local>
2013-05-27 15:19:20 -0700

    loop!

--- scroll.js
+++ scroll.js
@@ -8,47 +8,14 @@ $(document).ready(function(){
 	console.log(First);
 	console.log((First[2]));
 
-$.each([52, 97], function(index, value) {
-  alert(index + ': ' + value);
-});
-
-
-	/* The Goal
-
-	for (var i=0; i<=First.length; i++) {
-		$(First[i]).click(function() {
-			$(this).hide();
-			$(First[i++]).show();
-		if (i == First.length)
-			{$(First[i]).click(function() {
-			$(this).hide();
-			$(First[0]).show();
-			})};
-		})};
-	*/
-
-	$(".img1").click(function() {
-	  $(this).hide();
-	  $(".img2").show();
-		});
-	$(".img2").click(function() {
-	  $(this).hide();
-	  $(".img3").show();
-		});
-	$(".img3").click(function() {
-	  $(this).hide();
-	  $(".img4").show();
-		});
-	$(".img4").click(function() {
-	  $(this).hide();
-	  $(".img5").show();
-		});
-	$(".img5").click(function() {
-	  $(this).hide();
-	  $(".img1").show();
-		});
-});
+	$.each([[".img1", ".img2"], [".img2", ".img3"], [".img3", ".img4"]], function(index, value) {
+	  $(value[0]).click(function() {
+	  	$(this).hide();
+	  	$(value[1]).show();
+	 	});
+	});
  
+});
 
 //need to scale for other thumbnails?
 //need a condition that checks if it's the last image - loop back to #1
