8d2fb6aab99ba69014b9ef7b245a50518950d618
Karen <karen@Karens-MacBook-Air.local>
2013-05-27 17:28:37 -0700

    sliced arrays ready to zip

--- scroll.js
+++ scroll.js
@@ -5,16 +5,33 @@ $(document).ready(function(){
 	$("#first img").each(function(){
 		First.push($(this).attr("class"))});
 
-	console.log(First);
-	console.log((First[2]));
+	//console.log(First);
+
+	//["img1", "img2", "img3", "img4", "img5"]
+	//[".img1", ".img2", ".img3", ".img4", ".img5"]
+	//[[".img1", ".img2"], [".img2", ".img3"], [".img3", ".img4"], [".img4", ".img5"], [".img5", ".img1"]]
+	var FirstArr = [];
+	var FirstArr2 = [];
+
+	$.each(First, function(index, value) {
+  		FirstArr.push("."+value);
+		});
+
+	var FirstArr2 = FirstArr.slice(0);
+	var FirstVal  = FirstArr2.shift();
+	FirstArr2.push(FirstVal);
+
+	console.log(FirstArr);
+	console.log(FirstArr2);
+
 
 	$.each([[".img1", ".img2"], [".img2", ".img3"], [".img3", ".img4"]], function(index, value) {
 	  $(value[0]).click(function() {
-	  	$(this).hide();
-	  	$(value[1]).show();
-	 	});
+		$(this).hide();
+		$(value[1]).show();
+		});
 	});
- 
+
 });
 
 //need to scale for other thumbnails?
